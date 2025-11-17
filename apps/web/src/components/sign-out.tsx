fc5e393c52b43a789dacd45dfdf3e8df36dd1bbc
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-11-17 16:23:53 +0530

    feat(web): add theme switch and sign out components

---
+++ apps/web/src/components/sign-out.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { signOut } from "@/lib/auth/client";
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+
+export function SignOut() {
+  const [isLoading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    setLoading(true);
+
+    await signOut();
+
+    router.push("/login");
+  };
+
+  return (
+    <DropdownMenuItem onClick={handleSignOut}>
+      {isLoading ? "Loading..." : "Sign out"}
+    </DropdownMenuItem>
+  );
+}
---
+++ apps/web/src/components/theme-switch.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Monitor, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type Theme = "dark" | "system" | "light";
+
+type Props = {
+  currentTheme?: Theme;
+};
+
+const ThemeIcon = ({ currentTheme }: Props) => {
+  switch (currentTheme) {
+    case "dark":
+      return <Moon size={12} />;
+    case "system":
+      return <Monitor size={12} />;
+    default:
+      return <Sun size={12} />;
+  }
+};
+
+export const ThemeSwitch = () => {
+  const { theme, setTheme, themes, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return <div className="h-[32px]" />;
+  }
+
+  return (
+    <div className="relative flex items-center">
+      <Select onValueChange={(value: Theme) => setTheme(value)} value={theme}>
+        <SelectTrigger className="h-[32px] w-full bg-transparent py-1.5 pr-3 pl-6 text-xs capitalize outline-none">
+          <SelectValue>
+            {theme
+              ? theme.charAt(0).toUpperCase() + theme.slice(1)
+              : "Select theme"}
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {themes.map((_theme) => (
+              <SelectItem className="capitalize" key={_theme} value={_theme}>
+                {_theme}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+      <div className="pointer-events-none absolute left-2">
+        <ThemeIcon currentTheme={resolvedTheme as Theme} />
+      </div>
+    </div>
+  );
+};
