40a25fe52496ebcf3a6090ec5c5d7235b5caef4c
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-11-17 16:20:41 +0530

    feat(db): export database connection utilities and type

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,4 +2,8 @@ import { PrismaClient } from "../prisma/generated/client";
 
 const prisma = new PrismaClient();
 
+export const connectDb = async () => prisma;
+
+export type Database = PrismaClient;
+
 export default prisma;
