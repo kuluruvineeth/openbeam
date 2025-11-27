22e6d88c31ad1c6484043651f0be2669d71748a6
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-11-27 23:35:23 +0530

    chore: refine CI/CD workflow for manual triggers and add cleanup policies in Terraform

--- infra/terraform/modules/artifact-registry/main.tf
+++ infra/terraform/modules/artifact-registry/main.tf
@@ -21,4 +21,24 @@ resource "google_artifact_registry_repository" "repo" {
       managed_by = "terraform"
     }
   )
+
+  # Cleanup policy: Keep only latest + last 3 versions
+  cleanup_policies {
+    id     = "keep-latest"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 4
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+
+    condition {
+      tag_state = "UNTAGGED"
+      older_than = "604800s" # 7 days
+    }
+  }
 }
