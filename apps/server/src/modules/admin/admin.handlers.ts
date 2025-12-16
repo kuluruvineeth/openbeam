1580212385add2ad2a8ef84f4e995d68e1719e99
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-12-16 18:40:53 +0530

    chore: update admin handlers and pre-commit hook

--- apps/server/src/modules/admin/admin.handlers.ts
+++ apps/server/src/modules/admin/admin.handlers.ts
@@ -3,6 +3,7 @@ import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 import { HonoAdapter } from "@bull-board/hono";
 import {
   cleanupQueue,
+  connectorCleanupQueue,
   digestQueue,
   fileProcessingQueue,
   indexQueue,
@@ -25,6 +26,7 @@ export const setupBullBoard = () => {
       new BullMQAdapter(fileProcessingQueue),
       new BullMQAdapter(mediaProcessingQueue),
       new BullMQAdapter(digestQueue),
+      new BullMQAdapter(connectorCleanupQueue),
     ],
     serverAdapter,
   });
