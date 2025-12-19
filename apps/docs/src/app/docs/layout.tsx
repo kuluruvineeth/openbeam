9141384048b2907fbf9baf0d5d74bb6eac13500a
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2025-12-19 21:03:06 +0530

    refactor(docs): rename fumadocs app to docs

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -4,7 +4,13 @@ import { source } from "@/lib/source";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions()}
+      sidebar={{
+        defaultOpenLevel: 1,
+      }}
+    >
       {children}
     </DocsLayout>
   );
