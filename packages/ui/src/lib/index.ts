c2b1bf9cb93712938de011fc2339621c122f6a77
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2026-01-16 22:39:01 +0530

    feat(ui): Add agent utilities and tool registry

---
+++ packages/ui/src/lib/index.ts
@@ -0,0 +1,11 @@
+export { AGENT_UI_CONSTANTS, type AgentUIConstants } from "./agent-constants";
+export {
+  DEFAULT_TOOL,
+  getStatusIcon,
+  getToolCategory,
+  getToolDisplayName,
+  getToolIcon,
+  getToolMetadata,
+  TOOL_REGISTRY,
+  type ToolMetadata,
+} from "./tool-registry";
