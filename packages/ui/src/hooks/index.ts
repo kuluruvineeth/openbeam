219def1f4f6d81596453a1314a81bcba7963825d
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2026-01-16 22:40:41 +0530

    feat(ui): Export agent components and hooks from package

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,1 +1,40 @@
-export {};
+export type {
+  DoneEvent,
+  ErrorEvent,
+  StatusEvent,
+  StreamEvent,
+  StreamEventType,
+  StreamStatus,
+  TextEvent,
+  ThinkingEvent,
+  ToolCallEvent,
+  ToolResultEvent,
+  UseAgentStreamOptions,
+  UseAgentStreamReturn,
+} from "./use-agent-stream";
+export { useAgentStream } from "./use-agent-stream";
+export { useAutoCollapse } from "./use-auto-collapse";
+export { useAutoScroll } from "./use-auto-scroll";
+export type {
+  AgentEvent,
+  EventGroup,
+  GroupedEventItem,
+  UseEventGroupingReturn,
+} from "./use-event-grouping";
+export {
+  EXPLORATION_TOOLS,
+  isExplorationTool,
+  useEventGrouping,
+} from "./use-event-grouping";
+export {
+  EMPTY_THINKING_STATE,
+  type ThinkingState,
+  type UseThinkingReturn,
+  useThinking,
+} from "./use-thinking";
+export type {
+  ToolStep,
+  ToolStepStatus,
+  UseToolStepsReturn,
+} from "./use-tool-steps";
+export { useToolSteps } from "./use-tool-steps";
