469da818eebc40d0d76563a7f1ce0644d0273e0f
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2026-01-21 09:45:39 +0530

    chore(web): remove empty feature placeholder directories

--- apps/web/src/features/index.ts
+++ apps/web/src/features/index.ts
@@ -1,8 +1,6 @@
-export * as agents from "./agents";
 export * as chat from "./chat";
 export * as connectors from "./connectors";
 export * as contentPreview from "./content-preview";
-export * as dataSources from "./data-sources";
 export * as filePreview from "./file-preview";
 export * as overview from "./overview";
 export * as search from "./search";
