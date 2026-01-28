847fb34e6bcc742481dbcd7c5a88c9da68798e21
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2026-01-28 16:32:10 +0530

    feat(ui): add tool-elements sub-components for parameter binding

---
+++ packages/ui/src/components/canvas/tool-elements/index.ts
@@ -0,0 +1,10 @@
+export { BindingModeToggle } from "./binding-mode-toggle";
+export {
+  ParameterBindingEditor,
+  type ToolParameterDef,
+} from "./parameter-binding-editor";
+export {
+  ToolPicker,
+  type ToolPickerItem,
+  type ToolPickerProps,
+} from "./tool-picker";
