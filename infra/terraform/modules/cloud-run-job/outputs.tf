27a45180db161f9dad34eb6b18f5b33033d2427c
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2026-03-11 14:54:57 +0530

    chore(infra): Remove legacy GCP Terraform modules

--- infra/terraform/modules/cloud-run-job/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "job_name" {
-  description = "The name of the Cloud Run Job"
-  value       = google_cloud_run_v2_job.job.name
-}
-
-output "job_id" {
-  description = "The full resource ID of the job"
-  value       = google_cloud_run_v2_job.job.id
-}
